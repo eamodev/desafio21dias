ebb707e569262d059165e2d049c54e10caa9cfeb
eamodev <eduard4amoriim@gmail.com>
2025-11-26 19:35:03 -0300

    Estudo de variaveis e tipos de dados

---
+++ dia3.py
@@ -0,0 +1,15 @@
+#variaveis e tipos de dados
+
+#forma na qual você da define os campos e valores deles
+#nome = "Eduarda"
+#idade= 23
+#altura = 1.60
+#developer = True
+#print(f"Olá, meu nome é {nome}, tenho {idade} anos e tenho  {altura} de altura")
+
+
+#forma na qual o usuario define os campos e valores deles
+nome = input("Qual seu nome?: ")
+idade = int (input("Qual sua idade?: "))
+altura = float (input("Qual sua altura?: "))
+print(f"Olá, {nome}! Você tem {idade} anos e mede {altura}m.")
